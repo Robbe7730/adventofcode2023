de68abe62d18797a21ce389a9bbb0569ba1c5a85
Robbe Van Herck <robbe@robbevanherck.be>
2023-12-05 17:58:22 +0100

    Restructure to make a cleaner build

---
+++ build.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+const allocator = std.heap.page_allocator;
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const DAYS_IMPLEMENTED = 5;
+
+    for (1..(DAYS_IMPLEMENTED+1)) |day| {
+        var name: []u8 = "";
+        if (day < 10) {
+             name = std.fmt.allocPrint(
+                allocator,
+                "day0{}",
+                .{day}
+            ) catch unreachable;
+        } else {
+             name = std.fmt.allocPrint(
+                allocator,
+                "day{}",
+                .{day}
+            ) catch unreachable;
+        }
+        const exe = b.addExecutable(.{
+            .name = name,
+            .root_source_file = .{
+                .path = std.fmt.allocPrint(
+                    allocator,
+                    "src/{s}.zig",
+                    .{name}
+                ) catch unreachable
+            },
+            .target = target,
+            .optimize = optimize,
+        });
+        b.installArtifact(exe);
+    }
+}
