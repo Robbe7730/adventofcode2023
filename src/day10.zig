c24ed8dc9f069846c0ca9a95f28e3fc9e7081b4c
Robbe Van Herck <robbe@robbevanherck.be>
2023-12-19 17:30:57 +0100

    Day 10 part 2 but not working :'(

--- src/day10.zig
+++ src/day10.zig
@@ -133,7 +133,156 @@ fn part1(input: ArrayList(u8)) usize {
 }
 
 fn part2(input: ArrayList(u8)) usize {
-    _ = input;
     var ret: usize = 0;
+
+    var grid = ArrayList(ArrayList(u8)).init(allocator);
+    defer grid.deinit();
+    var new_line = ArrayList(u8).init(allocator);
+
+    var is_path = ArrayList(ArrayList(bool)).init(allocator);
+    defer is_path.deinit();
+    var path_line = ArrayList(bool).init(allocator);
+
+    var is_outside = ArrayList(ArrayList(bool)).init(allocator);
+    defer is_outside.deinit();
+    var outside_line = ArrayList(bool).init(allocator);
+
+    var start_pos: [2]usize = .{0, 0};
+    var x: usize = 0;
+    var y: usize = 0;
+
+    for (input.items) |tile| {
+        if (tile == 'S') {
+            start_pos = .{x, y};
+        }
+
+        if (tile == '\n') {
+            grid.append(new_line) catch unreachable;
+            is_path.append(path_line) catch unreachable;
+            is_outside.append(outside_line) catch unreachable;
+
+            new_line = ArrayList(u8).init(allocator);
+            path_line = ArrayList(bool).init(allocator);
+            outside_line = ArrayList(bool).init(allocator);
+
+            x = 0;
+            y += 1;
+        } else {
+            new_line.append(tile) catch unreachable;
+            path_line.append(false) catch unreachable;
+            outside_line.append(false) catch unreachable;
+
+            x += 1;
+        }
+    }
+
+    x = start_pos[0];
+    y = start_pos[1];
+
+    var dx: isize = 0;
+    var dy: isize = 0;
+
+    if (
+        grid.items[y-1].items[x] == '|' or
+        grid.items[y-1].items[x] == 'F' or
+        grid.items[y-1].items[x] == '7'
+    ) {
+        // Move north
+        dy = -1;
+    } else if (
+        grid.items[y].items[x+1] == '-' or
+        grid.items[y].items[x+1] == 'J' or
+        grid.items[y].items[x+1] == '7'
+    ) {
+        // Move east
+        dx = 1;
+    } else if (
+        grid.items[y+1].items[x] == '|' or
+        grid.items[y+1].items[x] == 'J' or
+        grid.items[y+1].items[x] == 'L'
+    ) {
+        // Move south
+        dy = 1;
+    } else if (
+        grid.items[y].items[x-1] == '-' or
+        grid.items[y].items[x-1] == 'L' or
+        grid.items[y].items[x-1] == 'F'
+    ) {
+        // Move west
+        dx = -1;
+    }
+
+    is_path.items[y].items[x] = true;
+    x = @as(usize, @intCast(@as(isize, @intCast(x)) + dx));
+    y = @as(usize, @intCast(@as(isize, @intCast(y)) + dy));
+
+    while (x != start_pos[0] or y != start_pos[1]) {
+        is_path.items[y].items[x] = true;
+        const new_ds = find_ds(dx, dy, grid.items[y].items[x]);
+
+        dx = new_ds[0];
+        dy = new_ds[1];
+
+        x =  @as(usize, @intCast(@as(isize, @intCast(x)) + dx));
+        y =  @as(usize, @intCast(@as(isize, @intCast(y)) + dy));
+    }
+
+    var queue = ArrayList([2]usize).init(allocator);
+    queue.append(.{0, 0}) catch unreachable;
+
+    while (queue.items.len > 0) {
+        const pos = queue.pop();
+        x = pos[0];
+        y = pos[1];
+        if (!is_outside.items[y].items[x] and !is_path.items[y].items[x]){
+            is_outside.items[y].items[x] = true;
+            if (x >= 1) {
+                queue.append(.{x-1, y}) catch unreachable;
+
+                if (y >= 1) {
+                    queue.append(.{x-1, y-1}) catch unreachable;
+                }
+                if (y < is_outside.items.len-1) {
+                    queue.append(.{x-1, y+1}) catch unreachable;
+                }
+            }
+            if (x < is_outside.items[0].items.len-1) {
+                queue.append(.{x+1, y}) catch unreachable;
+
+                if (y >= 1) {
+                    queue.append(.{x+1, y-1}) catch unreachable;
+                }
+                if (y < is_outside.items.len-1) {
+                    queue.append(.{x+1, y+1}) catch unreachable;
+                }
+            }
+            if (y >= 1) {
+                queue.append(.{x, y-1}) catch unreachable;
+            }
+            if (y < is_outside.items.len-1) {
+                queue.append(.{x, y+1}) catch unreachable;
+            }
+        }
+    }
+
+    for (0..is_path.items.len) |y1| {
+        path_line = is_path.items[y1];
+        outside_line = is_outside.items[y1];
+        for (0..path_line.items.len) |x1| {
+            if (path_line.items[x1]) {
+                std.debug.print(".", .{});
+            } else if (outside_line.items[x1]) {
+                std.debug.print("O", .{});
+            } else {
+                std.debug.print("I", .{});
+                ret += 1;
+            }
+        }
+        std.debug.print("\n", .{});
+    }
+    std.debug.print("\n", .{});
+
+    // 692 --> too high
+    // 684 --> too high
     return ret;
 }
